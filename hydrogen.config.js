483c3dcf1dc3f9a124c9181127f9b2f48b95ad7c
Keith Greene <keith.greene@canopygrowth.com>
2022-11-01 09:46:33 -0400

    add the files

---
+++ hydrogen.config.js
@@ -0,0 +1,23 @@
+import {
+  defineConfig,
+  CookieSessionStorage,
+  PerformanceMetricsServerAnalyticsConnector,
+} from '@shopify/hydrogen/config';
+
+export default defineConfig({
+  shopify: () => ({
+    defaultLanguageCode: 'EN',
+    defaultCountryCode: 'CA',
+    storeDomain: 'keith-tweed.myshopify.com',
+    storefrontToken: '0b5dc3f1269ae6b90d7dd53adb0c42db',
+    storefrontApiVersion: '2022-07',
+  }),
+  session: CookieSessionStorage('__session', {
+    path: '/',
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'strict',
+    maxAge: 60 * 60 * 24 * 30,
+  }),
+  serverAnalyticsConnectors: [PerformanceMetricsServerAnalyticsConnector],
+});
